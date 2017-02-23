6b7c479e6b61b56ac73fc0bdd6384b91e056e9a4
mousewip <mousewip@gmail.com>
2017-02-23 19:59:20 +0700

    Add project files.

---
+++ Model/Dao/UserDao.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.EF;
+
+namespace Model.Dao
+{
+    public class UserDao
+    {
+        OnlineShopDbContext db = null;
+        public UserDao()
+        {
+            db = new OnlineShopDbContext();
+        }
+
+        public long Insert(User entity)
+        {
+            db.Users.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+        public User GetByID(string userName)
+        {
+            return db.Users.SingleOrDefault(x => x.UserName == userName);
+        }
+
+        public int Login(string userName, string password)
+        {
+            var result = db.Users.SingleOrDefault(x => x.UserName == userName);
+            if(result == null)
+            {
+                // 0 = tk khong ton tai
+                return 0;
+            }
+            else
+            {
+                if(result.Status == false)
+                {
+                    // -1 = tk bi khoa
+                    return -1;
+                }
+                else
+                {
+                    if (result.Password == password)
+                        // 1 = password dung
+                        return 1;
+                    else
+                        // 2 = password sai
+                        return 2;
+                }
+                
+            }
+        }
+    }
+}
